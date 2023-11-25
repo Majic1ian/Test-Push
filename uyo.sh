8e40107568bb66438eb86f416170f8fac2f072df
Cloud User <ec2-user@ip-172-31-44-19.us-east-2.compute.internal>
2023-11-25 16:53:12 +0000

    Finally delivered

---
+++ uyo.sh
@@ -0,0 +1,10 @@
+name=Uyodhu
+cpy=DSS
+city=Calabar
+echo my name is $name  
+echo i work for $cpy  
+echo we are located in $city
+echo i kill people for free
+echo ${name} works for $cpy located in $city
+
+
